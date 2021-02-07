391a001c2a4c0c57a14ed01b0107254d916e98eb
cherjios <slopez0286@gmail.com>
2021-02-07 14:20:01 -0800

    updating

--- JavaScript/min on array.js
+++ JavaScript/min on array.js
@@ -6,7 +6,6 @@
 function soluction (A){
 
     let min  = Math.min(...A);
-    let max  = Math.max(...A);
     let NoPresentN = min + 1;
     let nfound =  true;
     
@@ -35,8 +34,10 @@ let Array1 = [1,2,3,5]; // Result 4
 let Array2 = [1,2,3,4]; // Result 5
 let Array3 = [-1,-2,-3]; // Result 1
 let Array4 = [-1,-2,1]; // Result 2
+let Array5 = [1,3,5,7,9,1,8]; // Result 2
 console.log("El min value no presented in Array " + Array + " is "+ soluction(Array));
 console.log("El min value no presented in Array " + Array1 + " is "+ soluction(Array1));
 console.log("El min value no presented in Array " + Array2 + " is "+ soluction(Array2));
 console.log("El min value no presented in Array " + Array3 + " is "+ soluction(Array3));
 console.log("El min value no presented in Array " + Array4 + " is "+ soluction(Array4));
+console.log("El min value no presented in Array " + Array5 + " is "+ soluction(Array5));
